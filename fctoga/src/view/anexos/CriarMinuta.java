ffa45248dd60e853191086bcd65185ea3cdf7eba
GuiCT <gtomiasi@gmail.com>
2023-01-27 14:39:43 -0300

    fix: limitando tipos de Minuta

--- fctoga/src/view/anexos/CriarMinuta.java
+++ fctoga/src/view/anexos/CriarMinuta.java
@@ -4,9 +4,10 @@ import controllers.FCToga;
 import models.Processo;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class CriarMinuta {
-    private static final String[] LABELS = {"Tipo da minuta", "Texto da minuta"};
+    private static final String[] TIPOS_MINUTA = {"Despacho", "Decisão", "Sentença"};
 
     public static JFrame render(Processo processo) {
         JFrame frame = new JFrame("Criar Minuta");
@@ -14,49 +15,51 @@ public class CriarMinuta {
         frame.setSize(400, 900);
 
         // ===== CAMPOS =====
-        JLabel[] labels = new JLabel[2];
-        JTextField[] textFields = new JTextField[2];
-        for (int i = 0; i < labels.length; i++) {
-            labels[i] = new JLabel(LABELS[i]);
-            textFields[i] = new JTextField();
-        }
+        // Tipo da minuta
+        JLabel tipoLabel = new JLabel("Tipo da minuta");
+        JComboBox<String> tipoField = new JComboBox<>();
+        Arrays.stream(TIPOS_MINUTA).forEach(tipoField::addItem);
+        Box tipoBox = Box.createHorizontalBox(); tipoBox.add(tipoLabel); tipoBox.add(tipoField);
+
+        // Texto da minuta
+        JLabel textoLabel = new JLabel("Texto da minuta");
+        JTextArea textoField = new JTextArea();
+        Box textoBox = Box.createHorizontalBox(); textoBox.add(textoLabel); textoBox.add(textoField);
 
         // Botão de adicionar
         JButton adicionar = new JButton("Criar minuta");
         adicionar.addActionListener(e -> {
-            for (JTextField campo : textFields) {
-                if (campo.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(frame, "Preencha todos os campos", "Erro", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
+            if (tipoField.getSelectedItem() == null) {
+                JOptionPane.showMessageDialog(frame, "Selecione um tipo de minuta");
+                return;
+            }
+
+            if (textoField.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(frame, "Digite o texto da minuta");
+                return;
             }
 
             // Adiciona minuta
-            processo.adicionarMinuta(textFields[0].getText(), textFields[1].getText());
+            processo.adicionarMinuta(tipoField.getSelectedItem().toString(), textoField.getText());
             FCToga.serializeInstance();
             JOptionPane.showMessageDialog(frame, "Minuta adicionada com sucesso");
             frame.dispose();
         });
+        Box adicionarBox = Box.createHorizontalBox(); adicionarBox.add(adicionar);
 
         // Adiciona componentes ao frame
         // Box layout horizontal para cada campo
         // Box layout vertical para o frame em si
-        Box verticalBox = Box.createVerticalBox();
-        for (int i = 0; i < textFields.length; i++) {
-            Box box = Box.createHorizontalBox();
-            box.add(labels[i]);
-            box.add(Box.createHorizontalStrut(10));
-            box.add(textFields[i]);
-            verticalBox.add(box);
-            verticalBox.add(Box.createVerticalStrut(10));
-        }
+        Box frameBox = Box.createVerticalBox();
+        frameBox.add(Box.createVerticalStrut(10));
+        frameBox.add(tipoBox);
+        frameBox.add(Box.createVerticalStrut(10));
+        frameBox.add(textoBox);
+        frameBox.add(Box.createVerticalStrut(10));
+        frameBox.add(adicionarBox);
+        frameBox.add(Box.createVerticalStrut(10));
 
-        // Horizontal box para o botão
-        Box buttonBox = Box.createHorizontalBox();
-        buttonBox.add(adicionar);
-        verticalBox.add(buttonBox);
-
-        frame.getContentPane().add(verticalBox);
+        frame.getContentPane().add(frameBox);
         return frame;
     }
 }
